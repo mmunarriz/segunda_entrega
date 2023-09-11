3a69e01f49429d28ef138af79d99641402f9e15a
Mariano Munarriz <mmunarriz@gmail.com>
2023-09-10 21:02:39 -0300

    add view '/products' para visualizar los productos

--- src/routes/views.js
+++ src/routes/views.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import productsModel from "../dao/models/products.js";
 
 const router = Router();
 
 router.get('/', (req, res) => {
     res.render('chat', {});
+})
+
+router.get('/products', async (req,res)=>{
+    const {page=1} = req.query;
+    const {docs,hasPrevPage,hasNextPage,nextPage,prevPage} = await productsModel.paginate({},{limit:10,page,lean:true});
+    const products = docs;
+    res.render('products',{
+        products,
+        hasPrevPage,
+        hasNextPage,
+        prevPage,
+        nextPage
+    });
 })
 
 export default router
